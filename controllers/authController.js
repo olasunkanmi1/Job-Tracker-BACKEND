fb2945e0bf28c66a8a5a330a0649571ae8cdd1cf
Olasunkanmi <abdulsalamquadri999@gmail.com>
2022-12-18 02:59:35 +0100

    initialize project

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+
+const register = async (req, res) => {
+    res.send('Register User')
+}
+
+const login = async (req, res) => {
+    res.send('login User')
+}
+
+const update = async (req, res) => {
+    res.send('update User')
+}
+
+export { register, login, update }
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+//db and authenticateUser
+import connectDB from './db/connect.js';
+
+//routers
+import authRouter from './routes/authRoutes.js';
+import jobsRouter from './routes/jobsRoutes.js';
+
+const app = express();
+dotenv.config()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome!')
+})
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
