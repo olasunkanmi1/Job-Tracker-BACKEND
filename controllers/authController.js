ce42453af45d240504e4bada27ac61908a3ec43c
Olasunkanmi <abdulsalamquadri999@gmail.com>
2022-12-20 16:52:22 +0100

    set up update user controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,7 @@ const login = async (req, res) => {
     if(!user) {
         throw new UnAuthenticatedError('Invalid Credentials');
     }
-    console.log(user)
+    // console.log(user)
 
     const isPasswordCorrect = await user.comparePassword(password)
     if(!isPasswordCorrect) {
@@ -57,8 +57,26 @@ const login = async (req, res) => {
 }
 
 const update = async (req, res) => {
-    console.log(req.user)
-    res.send('update User')
+    const { email, name, lastName, location } = req.body;
+
+    if(!email || !name || !lastName || !location) {
+        throw new BadRequestError('please provide all values');
+    }
+
+    const user = await User.findOne({ _id: req.user.userId })
+    user.email = email
+    user.name = name
+    user.lastName = lastName
+    user.location = location
+
+    await user.save()
+    const token = user.createJWT()
+
+    res.status(StatusCodes.OK).json({
+        user, 
+        token, 
+        location: user.location
+    })
 }
 
 export { register, login, update }
